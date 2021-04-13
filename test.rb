dfbada6c255a6a0bca741974c314f4183e25eb27
Andrew Brown <drusepth@gmail.com>
2021-04-13 15:38:26 -0700

    add file to test with so I don't have to keep building this gem overa nd over

---
+++ test.rb
@@ -0,0 +1,7 @@
+require_relative './lib/kernel_extensions'
+require_relative './lib/method_extensions'
+require_relative './lib/inline_tests'
+require_relative './lib/inline_test_failure'
+
+require 'pry'
+binding.pry
